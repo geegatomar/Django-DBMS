1e3665f74e73090faa613fe7ab07c84420385630
Joshitha <djoshithareddy@gmail.com>
2021-03-23 01:16:21 +0530

    A database containing items available and a form to sell items has been made.

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 # creating a table in the database
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    # text field gives unlimited text space
-    content = models.TextField()
+class Items(models.Model):
+
+    class ItemType(models.TextChoices):
+        DRAFTERS = 'DRAFTERS',_('Drafters')
+        BOOKS = 'BOOKS',_('Books')
+        CYCLES = 'CYCLES',_('Cycles')
+        OTHER = 'OTHER',_('Other (Mattress,Mirror,Laundry Baskets/Hangers,etc')
+    
+    item_name = models.CharField(max_length=100)
+    item_type = models.CharField(max_length=10,
+                                 choices=ItemType.choices,
+                                 default=ItemType.OTHER,
+                                )
+    item_details = models.TextField()
+    item_price = models.FloatField(default=0.00)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self): 
+        return self.item_name 
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Items
+from .forms import ItemsForm
 
 # Create your views here.
 
@@ -11,3 +13,14 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+def create_view(request): 
+    context ={} 
+
+    form = ItemsForm(request.POST or None) 
+    if form.is_valid(): 
+        form.save() 
+          
+    context['form']= form 
+    return render(request,'blog/sell.html',context)
