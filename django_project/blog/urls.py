7668825b2edee1c927bff0a979fc8e927b6b1bc9
Joshitha <djoshithareddy@gmail.com>
2021-03-26 23:19:00 +0530

    Displays all the items and search bar implemented with a few issues

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    #path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('sell/',views.create_view,name='blog-sell')
+    path('sell/',views.create_view,name='blog-sell'),
+    path('', views.list_view,name='blog-home'),
+    path('search/',views.search,name='search')
 ]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -6,7 +6,7 @@ from .form import UserRegisterForm
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == 'POST':    
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
