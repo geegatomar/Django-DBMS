f42677fe0530d1eb5134b260fba45aa93dbb23e8
Joshitha <djoshithareddy@gmail.com>
2021-03-31 11:46:55 +0530

    Added email and fixed some minor bugs

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -8,7 +8,10 @@ from django.urls import reverse
 from .models import Items, ItemsCart
 from .forms import ItemsForm,ItemsCartForm
 from django.db.models import Q
+from django_project.settings import EMAIL_HOST_USER
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, 'home.html')
@@ -20,13 +23,12 @@ def about(request):
 
 def create_view(request): 
     context ={} 
-
     form = ItemsForm(request.POST or None) 
     if form.is_valid(): 
         item = form.save(commit = False)
         item.author = request.user   
-        item.save()  
-          
+        item.save()   
+        return HttpResponseRedirect("/")        
     context['form']= form 
     return render(request,'blog/sell.html',context)
 
@@ -38,18 +40,13 @@ def list_view(request):
 def search(request):
     if request.method=="GET":
         query=request.GET.get('search')
-        #results=Items.objects.all().filter(item_name=query).values()
-        results=Items.objects.all().raw("Select * from blog_Items where item_name=%s",[query])
+        results=Items.objects.all().filter(item_name=query).values()
+        #results=Items.objects.all().raw("Select * from blog_Items where item_name='%s'",[query])
 
     return render(request,"blog/search.html",{'query':query,'results':results})
 
-# pass id attribute from urls
 def detail_view(request, id):   
-    # dictionary for initial data with 
-    # field names as keys
     context ={}
-  
-    # add the dictionary during initialization
     context["data"] = Items.objects.get(id = id)
           
     return render(request, "blog/itemdetails.html", context)
@@ -78,13 +75,24 @@ def buy(request, id):
     obj = get_object_or_404(Items, id = id)
     name = Items.objects.all().filter(id=id).values()
     k=name[0]['author_id']
+    m=name[0]['item_name']
     context = {}
     context['dataset'] = User.objects.all().filter(id=k).values()
+    con = User.objects.all().filter(id=k).values()
+    c=con[0]['email']
+    print(c)
+    
     form = ItemsCartForm(request.POST or None) 
     if form.is_valid(): 
         cart=ItemsCart.objects.get_or_create(item_id=Items.objects.get(id=id), buyer_id=request.user)
         update=Items.objects.filter(id=id).update(status=0)
-            
+        subject = 'welcome to GFG world'
+        message = f'Hi {request.user}  wants to buy the item {m} which you put up for sale. If you are not contacted by them here are their details: email: {request.user.email}'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [c]
+        print(recipient_list)
+        send_mail( subject, message, email_from, recipient_list )      
+        return HttpResponseRedirect("/")      
     return render(request, "blog/buy.html", context)
 
 def cart(request, id):
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -28,6 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "dminiproject@gmail.com"
+EMAIL_HOST_PASSWORD = "Django-dbms"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 # Application definition
 
 INSTALLED_APPS = [
